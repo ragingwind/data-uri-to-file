e713c4064534fd56b0b78b0b0a02b80603e2c3ca
ragingwind <ragingwind@gmail.com>
2016-07-10 09:00:00 +0900

    No babel

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
 'use strict';
 
-var _dataUriRegex = require('data-uri-regex');
-
-var _dataUriRegex2 = _interopRequireDefault(_dataUriRegex);
-
-var _mime = require('mime');
-
-var _mime2 = _interopRequireDefault(_mime);
-
-var _through = require('through2');
-
-var _through2 = _interopRequireDefault(_through);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const re = require('data-uri-regex');
+const mime = require('mime');
+const through2 = require('through2');
 
 function toFile(input) {
-	return new Promise(function (resolve, reject) {
+	return new Promise((resolve, reject) => {
 		if (typeof input !== 'string') {
 			return reject(new TypeError('Input should be string as base64'));
 		}
 
-		var data = (0, _dataUriRegex2.default)().exec(input);
+		var data = re().exec(input);
 
 		return resolve(data ? {
 			mimeType: data[2],
 			encoding: data[3],
-			data: new Buffer(data[4], data[3] || 'utf8'),
-			extension: _mime2.default.extension(data[2])
+			data: new Buffer(data[4]),
+			extension: mime.extension(data[2])
 		} : undefined);
 	});
 }
 
 module.exports = toFile;
 
-module.exports.stream = function () {
-	return _through2.default.obj(function (chunk, enc, cb) {
+module.exports.stream = () => {
+	return through2.obj(function (chunk, enc, cb) {
 		var _stream = this;
-		toFile(chunk.toString()).then(function (image) {
+		toFile(chunk.toString()).then(image => {
 			_stream.push(image.data);
 			cb();
-		}).catch(function (e) {
-			cb(e);
+		}).catch(err => {
+			cb(err);
 		});
 	});
 };
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-import test from 'ava';
-import fn from './';
+'use strict';
+
 import fs from 'fs';
-import path from 'path';
+import test from 'ava';
 import through2 from 'through2';
+import fn from './';
 
 test('Return image from data-uri', t => {
 	const png = 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/Pgo8IURPQ1RZUEUgc3ZnIFBVQkxJQyAiLS8vVzNDLy9EVEQgU1ZHIDEuMS8vRU4iIAogICJodHRwOi8vd3d3LnczLm9yZy9HcmFwaGljcy9TVkcvMS4xL0RURC9zdmcxMS5kdGQiPgo8c3ZnIHZlcnNpb249IjEuMSIKICAgICB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj4KICA8ZGVzYz5tZW51PC9kZXNjPgogIDxnIGlkPSJtZW51Ij48cGF0aCBkPSJNMywxOGgxOHYtMkgzVjE4eiBNMywxM2gxOHYtMkgzVjEzeiBNMyw2djJoMThWNkgzeiIvPjwvZz4KPC9zdmc+Cgo=';
@@ -11,7 +12,6 @@ test('Return image from data-uri', t => {
 		t.is(file.mimeType, 'image/svg+xml');
 		t.is(file.encoding, 'base64');
 		t.is(file.extension, 'svg');
-		t.end();
 	});
 });
 
@@ -19,9 +19,8 @@ test('Return image from stream with data-uri', t => {
 	fs.createReadStream('./fixtures/base64encoded.svg')
 		.pipe(fn.stream())
 		.pipe(through2.obj(() => {
-			t.end();
+			t.pass();
 		})).on('error', e => {
 			t.fail(e.toString());
-			t.end();
 		});
 });
